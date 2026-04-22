0e7274c0a94b74c54feadb95617adba23dacfbd6
rrwrrioe <damir100609@gmail.com>
2026-04-22 19:57:50 +0300

    feat: add .yaml config parsing

--- cmd/config.go
+++ cmd/config.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"crypto/ed25519"
-	"log"
+	"errors"
 	"log/slog"
+	"os"
 
-	"github.com/caarlos0/env/v11"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 var logLevels = map[uint8]slog.Level{
@@ -16,33 +17,33 @@ var logLevels = map[uint8]slog.Level{
 }
 
 type System struct {
-	Port             string             `env:"SYSTEM_PORT" envDefault:"9090"`
-	ADNLPort         string             `env:"SYSTEM_ADNL_PORT" envDefault:"16167"`
-	AccessTokens     string             `env:"SYSTEM_ACCESS_TOKENS" envDefault:""`
+	Port             string             `yaml:"system_port" env:"SYSTEM_PORT" envDefault:"9090"`
+	ADNLPort         string             `yaml:"system_adnl_port" env:"SYSTEM_ADNL_PORT" envDefault:"16167"`
+	AccessTokens     string             `env:"SYSTEM_ACCESS_TOKENS" envDefault:"dev-token"`
 	Key              ed25519.PrivateKey `env:"SYSTEM_KEY" required:"false"`
-	LogLevel         uint8              `env:"SYSTEM_LOG_LEVEL" envDefault:"1"` // 0 - debug, 1 - info, 2 - warn, 3 - error
-	StoreHistoryDays int                `env:"SYSTEM_STORE_HISTORY_DAYS" envDefault:"90"`
+	LogLevel         uint8              `yaml:"system_log_level" env:"SYSTEM_LOG_LEVEL" envDefault:"1"` // 0 - debug, 1 - info, 2 - warn, 3 - error
+	StoreHistoryDays int                `yaml:"system_store_history_days" env:"SYSTEM_STORE_HISTORY_DAYS" envDefault:"90"`
 }
 
 type Metrics struct {
-	Namespace        string `env:"NAMESPACE" default:"ton-storage"`
-	ServerSubsystem  string `env:"SERVER_SUBSYSTEM" default:"mtpo-server"`
-	WorkersSubsystem string `env:"WORKERS_SUBSYSTEM" default:"mtpo-workers"`
-	DbSubsystem      string `env:"DB_SUBSYSTEM" default:"mtpo-db"`
+	Namespace        string `yaml:"namespace" env:"NAMESPACE" envDefault:"ton-storage"`
+	ServerSubsystem  string `yaml:"server_subsystem" env:"SERVER_SUBSYSTEM" envDefault:"mtpo-server"`
+	WorkersSubsystem string `yaml:"workers_subsystem" env:"WORKERS_SUBSYSTEM" envDefault:"mtpo-workers"`
+	DbSubsystem      string `yaml:"db_subsystem" env:"DB_SUBSYSTEM" envDefault:"mtpo-db"`
 }
 
 type TON struct {
 	MasterAddress string `env:"MASTER_ADDRESS" required:"true" envDefault:"UQB3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d0x0"`
-	ConfigURL     string `env:"TON_CONFIG_URL" required:"true" envDefault:"https://ton-blockchain.github.io/global.config.json"`
-	BatchSize     uint32 `env:"BATCH_SIZE" required:"true" envDefault:"100"`
+	ConfigURL     string `yaml:"config_url" env:"TON_CONFIG_URL" required:"true"`
+	BatchSize     uint32 `yaml:"batch_size" env:"BATCH_SIZE" required:"true" envDefault:"100"`
 }
 
 type Postgress struct {
-	Host     string `env:"DB_HOST" required:"true"`
-	Port     string `env:"DB_PORT" required:"true"`
-	User     string `env:"DB_USER" required:"true"`
-	Password string `env:"DB_PASSWORD" required:"true"`
-	Name     string `env:"DB_NAME" required:"true"`
+	Host     string `yaml:"db_host" env:"DB_HOST" required:"true"`
+	Port     string `yaml:"db_port" env:"DB_PORT" required:"true"`
+	User     string `yaml:"db_user" env:"DB_USER" required:"true"`
+	Password string `yaml:"db_password" env:"DB_PASSWORD" required:"true"`
+	Name     string `yaml:"db_name" env:"DB_NAME" required:"true"`
 }
 
 type Config struct {
@@ -52,26 +53,31 @@ type Config struct {
 	DB      Postgress
 }
 
-func loadConfig() *Config {
-	cfg := &Config{}
-	if err := env.Parse(&cfg.System); err != nil {
-		log.Fatalf("Failed to parse system config: %v", err)
+func MustLoadConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		panic("config path is not set")
 	}
-	if err := env.Parse(&cfg.Metrics); err != nil {
-		log.Fatalf("Failed to parse metrics config: %v", err)
+
+	if _, err := os.Stat(configPath); errors.Is(err, os.ErrNotExist) {
+		panic("config file does not exist")
 	}
-	if err := env.Parse(&cfg.DB); err != nil {
-		log.Fatalf("Failed to parse db config: %v", err)
-	}
-	if err := env.Parse(&cfg.TON); err != nil {
-		log.Fatalf("Failed to parse TON config: %v", err)
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
 	}
 
 	if cfg.System.Key == nil {
-		_, priv, _ := ed25519.GenerateKey(nil)
+		_, priv, err := ed25519.GenerateKey(nil)
+		if err != nil {
+			panic("failed to generate private key: " + err.Error())
+		}
+
 		key := priv.Seed()
 		cfg.System.Key = ed25519.NewKeyFromSeed(key)
 	}
 
-	return cfg
+	return &cfg
 }
